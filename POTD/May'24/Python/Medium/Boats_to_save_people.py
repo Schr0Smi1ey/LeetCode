af0db03a35119d59c873750b6b78ef2efd836ebe
Schr0Sm1ley <radiantremel444@gmail.com>
2024-05-04 09:50:05 +0600

    POTD

---
+++ POTD/May'24/Python/Medium/Boats_to_save_people.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        i ,j , ans = 0,len(people) - 1,0
+        while i <= j:
+            if people[i] + people[j] <= limit:
+                ans += 1
+                i += 1
+                j -= 1
+            else:
+                ans += 1
+                j -= 1
+        return ans
